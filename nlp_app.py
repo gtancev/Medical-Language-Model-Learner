df3e165dd3c2c5b771190e9ed64a01d4d5effb33
Georgi Tancev <30585013+gtancev@users.noreply.github.com>
2019-11-11 08:00:31 +0100

    Add files via upload

--- nlp_app.py
+++ nlp_app.py
@@ -26,7 +26,7 @@ enc = LabelEncoder()
 def run():
     st.sidebar.text("© Georgi Tancev")
     st.title("Medical Language Model Learner (MLML)")
-    st.sidebar.header("Loading the data.")
+    st.sidebar.header("Loading data.")
     filename = st.sidebar.selectbox("Choose a file.",("None","mtsamples"))
 
     # LOAD DATA.
@@ -46,7 +46,7 @@ def run():
         "It is advisable to remove classes which are underrepresented and whose abundance is below some treshold to achieve a better classification performance.",
         "(Alternatively, try to collect more data.)")
         st.write("In addition, there are several degrees of freedom for the construction of the dictionary. Adjusting those parameters has an influence on the amount and kind of words (features) which are included in the model.")
-        st.subheader("Load some data.")
+        st.subheader("Load data.")
         st.write("Display sample data by ticking the checkbox in the sidebar.")
         #filename = st.sidebar.text_input('Enter the filename of a csv-file.')
 
@@ -66,7 +66,7 @@ def run():
         treshold_to_consider = st.sidebar.slider("Minimum fraction of class in initial data set.", min_value=0.01, max_value=0.1, value=0.06, step=0.01)
         classes_to_consider = unique_values[relative_counts >= treshold_to_consider]
 
-        index_to_consider = np.empty((labels.shape[0]),dtype="bool")
+        index_to_consider = np.empty((labels.shape[0]), dtype="bool")
         for i, label in enumerate(labels):
             if label in classes_to_consider:
                 index_to_consider[i] = True
@@ -84,10 +84,10 @@ def run():
         st.info("Some classes might be **underrepresented** in the data set. Consider removing them.")
         st.write("The final number of classes is", np.size(unique_values), " and the residual number of samples is", np.sum(index_to_consider), ".")
         rel_counts = pd.DataFrame(data=relative_counts, columns=["fraction of class in reduced data set"]).set_index(label_names)
-        st.table(rel_counts)
+        st.table(rel_counts.style.format("{:.2f}"))
 
         # DATA TRANSFORMATION
-        st.subheader("Transform the data.")
+        st.subheader("Transform data.")
         st.sidebar.header("Constructing dictonary of words.")
         if st.sidebar.checkbox("Use raw word count."):
             st.write("Transform the text data into **word count** representation.")
@@ -116,8 +116,8 @@ def run():
         with st.spinner('Data is being transformed.'):
             tfidf, feature_names = transform()
             st.success("Transformation finished.")
-            st.subheader("Visualize the data.")
-            st.write("Examine the distribution of classes by dimensionality reduction based on **singular value decomposition (SVD)**.")
+            st.subheader("Visualize data.")
+            st.write("Examine the distribution of classes by dimensionality reduction based on **latent semantic analysis**.")
             data_ = dimred.fit_transform(tfidf)
             data_ = pd.DataFrame(data=data_, columns=["principal component 1", "principal component 2"])
             labels_ = pd.DataFrame(data=enc.inverse_transform(labels), columns=["class"])
@@ -129,12 +129,15 @@ def run():
         # MODEL BUILDING.
         st.header("Training")
         st.write("The model is based on a **random forest**. Customize the model hyperparameters in the sidebar.")
-        st.sidebar.header("Customizing the model.")
+        st.sidebar.header("Customizing model.")
         n_estimators = st.sidebar.text_input('Number of trees in random forest.', '1000')
         max_leaf_nodes = st.sidebar.text_input('Maximum number of leaf nodes in a tree.', '25')
         max_depth = st.sidebar.text_input('Maximum depth of a tree.', '5')
-        class_weight = st.sidebar.selectbox("Class weights for the model.", ('balanced','balanced_subsample'))
-        forest_clf = RandomForestClassifier(n_estimators=int(n_estimators), max_depth=int(max_depth), max_leaf_nodes=int(max_leaf_nodes), class_weight=class_weight, oob_score=True, random_state=0) # Define classifier to optimize.
+        class_weight = st.sidebar.selectbox("Class weights for the model.", ('balanced','balanced subsample','none'))
+        cw = {}
+        cw["balanced"], cw["balanced subsample"], cw["none"] = "balanced", "balanced_subsample", None
+        forest_clf = RandomForestClassifier(n_estimators=int(n_estimators), max_depth=int(max_depth), max_leaf_nodes=int(max_leaf_nodes), 
+                                            class_weight=cw[class_weight], oob_score=True, random_state=0) # Define classifier to optimize.
         # parameters = {'max_leaf_nodes':np.linspace(20,35,14,dtype='int')} # Define grid. 
         # clf = RandomizedSearchCV(forest_clf, parameters, n_iter=10, cv=3,iid=False, scoring='accuracy',n_jobs=-1) # Balanced accuracy as performance measure.
 
@@ -186,28 +189,28 @@ def run():
 
         # PREDICTION
         st.header("Prediction")
-        st.subheader("Provide a document.")
+        st.subheader("Provide sample.")
         st.write("The model can now be used for **prediction** of the medical specialty.")
         
         desc = "CHIEF COMPLAINT: Non-healing surgical wound to the left posterior thigh. HISTORY OF PRESENT ILLNESS: This is a 49-year-old white male who sustained a traumatic injury to his left posterior thighthis past year while in ABCD. He sustained an injury from the patellar from a boat while in the water. He was air lifted actually up to XYZ Hospital and underwent extensive surgery. He still has an external fixation on it for the healing fractures in the leg and has undergone grafting and full thickness skin grafting closure to a large defect in his left posterior thigh, which is nearly healed right in the gluteal fold on that left area. In several areas right along the graft site and low in the leg, the patient has several areas of hypergranulation tissue. He has some drainage from these areas. There are no signs and symptoms of infection. He is referred to us to help him get those areas under control. PAST MEDICAL HISTORY: Essentially negative other than he has had C. difficile in the recent past. ALLERGIES: None. MEDICATIONS: Include Cipro and Flagyl. PAST SURGICAL HISTORY: Significant for his trauma surgery noted above. FAMILY HISTORY: His maternal grandmother had pancreatic cancer. Father had prostate cancer. There is heart disease in the father and diabetes in the father. SOCIAL HISTORY: He is a non-cigarette smoker and non-ETOH user.  He is divorced. He has three children. He has an attorney. PHYSICAL EXAMINATION: He presents as a well-developed, well-nourished 49-year-old white male who appears to be in no significant distress. IMPRESSION: Several multiple areas of hypergranulation tissue on the left posterior leg associated with a sense of trauma to his right posterior leg. PLAN: Plan would be for chemical cauterization of these areas. Series of treatment with chemical cauterization till these are closed."
-        text = st.text_area("Write down some input.", value=desc)
+        text = st.text_area("Write down some text.", value=desc)
         sample = tfidf_vectorizer.transform([text])
         pred_class = classifier.predict(sample).ravel()
         probability = np.round(np.max(classifier.predict_proba(sample).ravel()), 2)
         class_label = enc.inverse_transform(pred_class)[0]
         
-        st.subheader("Examine the result.")
+        st.subheader("Assess result.")
         st.write("This sample originates from the specialty **"+class_label+"** with a probability of", probability, ".")
         
     else:
         st.header("Introduction")
-        st.write("This application guides you through the development of a language model that classifies clinical documents according to their **medical specialty**.",
-        "It is based on a **term frequency–inverse document frequency (tf-idf)** approach; tf-idf is a numerical statistic that is intended to reflect how important a word is to a document in a collection or corpus.",
+        st.write("**This application guides you through the development of a language model that classifies clinical documents according to their medical specialty.**",
+        "It is based on a term frequency–inverse document frequency (tf-idf) approach; tf-idf is a numerical statistic that is intended to reflect how important a word is to a document in a collection or corpus.",
         "It is often used as a weighting factor in searches of information retrieval, text mining, and user modeling.",
         "The tf–idf value increases proportionally to the number of times a word appears in the document and is offset by the number of documents in the corpus that contain the word, which helps to adjust for the fact that some words appear more frequently in general.", 
         "tf–idf is one of the most popular term-weighting schemes today; 83% of text-based recommender systems in digital libraries use tf–idf. Note that tf-idf ignores the sequential aspect of a language.")
         
-        st.write("The actual model itself is based on a **random forest** classifier.",
+        st.write("The actual model itself is based on a random forest classifier.",
         "Random forests are an ensemble learning method for classification, regression and other tasks that operates by constructing a multitude of decision trees at training time and outputting the class that is the mode of the classes (classification) or mean prediction (regression) of the individual trees.",
         "In particular, trees that are grown very deep tend to learn highly irregular patterns: they overfit their training sets, i.e. have low bias, but very high variance. Random forests are a way of averaging multiple deep decision trees, trained on different parts of the same training set, with the goal of reducing the variance.",
         "Random forests can be used to rank the importance of variables in a regression or classification problem in a natural way.")
